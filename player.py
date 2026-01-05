f49068263ada8cdf7de1cfe9d1386edef2249910
BigBadJonnyT <greenyking@hotmail.com>
2026-01-05 18:03:56 +1100

    Added classes for circle and player objects, and code for drawing player on canvas.

---
+++ player.py
@@ -0,0 +1,23 @@
+import pygame
+from constants import LINE_WIDTH
+from circleshape import CircleShape
+
+class Player(CircleShape):
+    def __init__(self, x, y, radius):
+        super().__init__(x, y, radius)
+        self.rotation = 0
+
+    def triangle(self):
+        forward = pygame.Vector2(0, 1).rotate(self.rotation)
+        right = pygame.Vector2(0, 1).rotate(self.rotation + 90) * self.radius / 1.5
+        a = self.position + forward * self.radius
+        b = self.position - forward * self.radius - right
+        c = self.position - forward * self.radius + right
+        return [a, b, c]
+
+    def draw(self, screen):
+        color = "white"
+        points = self.triangle()
+        width = LINE_WIDTH
+        pygame.draw.polygon(screen, color, points, width)
+
