1b756dea66860310fbbe0b460eaef219357d4cec
BigBadJonnyT <greenyking@hotmail.com>
2026-01-05 21:51:02 +1100

    implemented collision logic

--- circleshape.py
+++ circleshape.py
@@ -20,3 +20,11 @@ class CircleShape(pygame.sprite.Sprite):
     def update(self, dt):
         # must override
         pass
+
+    def collides_with(self, other):
+        distance = self.position.distance_to(other.position)
+        collision_distance = self.radius + other.radius
+        if distance > collision_distance:
+            return False
+        else:
+            return True
