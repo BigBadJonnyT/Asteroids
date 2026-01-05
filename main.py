8e6ad9e9491c1b8422f8f9b3061706da21bc3342
BigBadJonnyT <greenyking@hotmail.com>
2026-01-05 22:31:01 +1100

    added player shooting functions

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 import pygame
-from constants import SCREEN_WIDTH, SCREEN_HEIGHT, PLAYER_RADIUS
+from constants import SCREEN_WIDTH, SCREEN_HEIGHT, PLAYER_RADIUS, SHOT_RADIUS
 from logger import log_state, log_event
 from player import Player
 from asteroid import Asteroid
 from asteroidfield import AsteroidField
+from shot import Shot
 import sys
 
 def main():
@@ -16,9 +17,11 @@ def main():
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
     asteroids = pygame.sprite.Group()
+    shots = pygame.sprite.Group()
     Player.containers = (updatable, drawable)
     Asteroid.containers = (asteroids, updatable, drawable)
     AsteroidField.containers = (updatable)
+    Shot.containers = (shots, updatable, drawable)
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     player = Player( SCREEN_WIDTH / 2, SCREEN_HEIGHT /2, PLAYER_RADIUS)
     asteroid_field = AsteroidField()
